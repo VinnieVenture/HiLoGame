88f045dbe192aeb5a58ff8aec4b2cb95ecd4f1cf
VinnieVenture <vinventurewow@gmail.com>
2022-12-25 22:32:31 +0100

    learn

--- HiLoGame/Program.cs
+++ HiLoGame/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HiLoGame;
+
+Console.WriteLine("Witaj w HiLo.");
+Console.WriteLine($"Odgadnij liczbę od 1 do {HiLoGameMecanic.MAXIMUM }.");
+HiLoGameMecanic.Hint();
+while (HiLoGameMecanic.GetPot() > 0)
+{
+    Console.WriteLine("Wciśnij w (większa), m (mniejsza) lub ? (kupno wskazówki).");
+    Console.WriteLine($"Inne klawisze to koniec gry z {HiLoGameMecanic.GetPot()}zł.");
+    char key = Console.ReadKey(true).KeyChar;
+    if (key == 'w') HiLoGameMecanic.Guess(true);
+    else if (key == 'm') HiLoGameMecanic.Guess(false);
+    else if (key == '?') HiLoGameMecanic.Hint();
+    else return;
+}
+Console.WriteLine("Skończyły Ci się pieniądze do zobaczenia!");
